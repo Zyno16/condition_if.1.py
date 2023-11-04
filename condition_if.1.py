8abffb4bff06a0d4e0ac423102fbe60e7b4502f2
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-04 10:12:48 +0000

    Add files via upload

---
+++ condition_if.1.py
@@ -0,0 +1,8 @@
+x = input("enter the number ? ")
+y = int(x)
+print ("the number is ",y)
+if y > 5:
+    print ("the number is less than 5")  
+else:
+    print ("the number is grather than 5")
+print ("all done")
